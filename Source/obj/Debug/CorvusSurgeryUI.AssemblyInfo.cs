350854b0cdca5d68dffc8b9366d11acbd0985fc8
PlatoArete <andrew.sproston@gmail.com>
2025-06-23 21:57:32 +0100

    Update to add new elements to UI drag not working wip

--- Source/obj/Debug/CorvusSurgeryUI.AssemblyInfo.cs
+++ Source/obj/Debug/CorvusSurgeryUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CorvusSurgeryUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fce00dc67ff45e014e0fd093ce42047a6707805")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39c23bc4ef860361574b69fe93d8ae919cde323b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CorvusSurgeryUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CorvusSurgeryUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
